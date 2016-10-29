eb24d113627e8e42db5da839d68a6421d12f413d
Jenner Hanni <jeh.wicker@gmail.com>
2016-10-29 11:05:36 -0700

    F_Cu vals are integer layer numbers.

--- scripting/create_mfr_files.py
+++ scripting/create_mfr_files.py
@@ -38,4 +38,60 @@ else:
 
 print filename
 
+board = LoadBoard(filename)
+
+plotDir = "gerbers/"
+
+pctl = PLOT_CONTROLLER(board)
+popt = pctl.GetPlotOptions()
+popt.SetOutputDirectory(plotDir)
+
+# Set plot options
+
+popt.SetPlotFrameRef(False)        # do not change it
+popt.SetLineWidth(FromMM(0.35))
+popt.SetAutoScale(False)           # do not change it
+popt.SetScale(1)                   # do not change it
+popt.SetMirror(False)
+popt.SetUseGerberAttributes(True)
+popt.SetUseGerberProtelExtensions(True)
+popt.SetExcludeEdgeLayer(False)
+popt.SetScale(1)
+popt.SetUseAuxOrigin(True)
+popt.SetSubtractMaskFromSilk(False)
+
+# this doesn't generate anything with F_Fab or F_SilkS
+# I want it on the Fab layer since that's where my assy info is
+# It might just be out of place
+#pctl.SetLayer(F_Fab)
+#pctl.OpenPlotfile("Fab", PLOT_FORMAT_PDF, "Assembly diagram")
+#pctl.PlotLayer()
+
+# The middle value is an integer layer number:
+# F.Cu      0
+# B.Cu      31
+# F.Paste   35
+# B.Paste   34
+# F.Silk    37
+# B.Silk    36
+# F.Mask    39
+# B.Mask    38
+# Edge.Cuts 44
+# F.Fab     49
+# B.Fab     48
+
+plot_plan = [
+    ( "F.Cu", F_Cu, "Top layer" ), 
+    ( "B.Cu", B_Cu, "Bottom layer" ),
+    ( "F.Paste", F_Paste, "Paste top" ),
+    ( "B.Paste", B_Paste, "Paste Bottom" ),
+    ( "F.Silk", F_SilkS, "Silk top" ),
+    ( "B.Silk", B_SilkS, "Silk top" ),
+    ( "F.Mask", F_Mask, "Mask top" ),
+    ( "B.Mask", B_Mask, "Mask bottom" ),
+    ( "Edge.Cuts", Edge_Cuts, "Board outline" ),
+    ( "F.Fab", F_Fab, "Assembly top" ),
+    ( "B.Fab", B_Fab, "Assembly bottom" ),
+]
+
 
